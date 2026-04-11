8105b6eadcccf7a91f202070095c9b5e22c7f9f7
Matthew Benedict <112194526+realMatthewBenedict@users.noreply.github.com>
2026-04-12 08:27:11 +0900

    Clean up main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from dataclasses import dataclass
 from enum import IntFlag
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
-import sklearn
 import tensorflow as tf
 
 class Category(IntFlag):
@@ -19,7 +17,7 @@ class FileResult:
     """Record containing all available data for an audio file"""
     file_name: str
     audio_tensor: tf.Tensor           # [samples, channels]
-    spectrogram_tensor: tf.Tensor     # [257, time_frames]
+    spectrogram_tensor: tf.Tensor     # [257, time_frames] (unless modified by convert_to_mel)
     spectrogram_bins: int             # 257
     sample_rate: int
     category: Category
@@ -27,10 +25,11 @@ class FileResult:
     rumble_frames: list[tuple[int, int]] # corresponds to spectrogram_tensor
     call_type: str
 
-def convert_time(row, sample_rate_int, frame_step) -> tuple[tuple]:
+def convert_time(row: pd.Series, audio_tensor: tf.Tensor, sample_rate_int: int, frame_step: int) -> tuple[tuple[int, int], tuple[int, int]]:
     # Note: Samples of audio, not spectrogram
     start_sample=int(row["Start_time"] * sample_rate_int)
     end_sample=int(row["End_time"] * sample_rate_int)
+    end_sample = min(end_sample, audio_tensor.shape[0] - 1)  # clip to waveform length
 
     # Note: Frames of spectrogram, not audio
     start_frame = int(start_sample / frame_step)
@@ -38,20 +37,25 @@ def convert_time(row, sample_rate_int, frame_step) -> tuple[tuple]:
 
     return ((start_sample, end_sample), (start_frame, end_frame))
 
+frame_length = 512
+frame_step = 256
+fft_length = 512
+
 def read_audio_files(index_path: Path, directory: Path) -> list[FileResult]:
     df = pd.read_csv(str(index_path), index_col="Selection")
-    result = []
+    result: dict[str, FileResult] = {}
 
+    # Only read files with indexed rumbles
     for index, row in df.iterrows():
         file_name = row["Sound_file"]
-        frame_step = 256
-        fft_length = 512
+        global frame_length, frame_step, fft_length
 
         # Assume CSV is sorted by file
-        if len(result) > 0 and result[-1].file_name == file_name:
-            sample, frame = convert_time(row, result[-1].sample_rate, frame_step)
-            result[-1].rumble_samples.append(sample)
-            result[-1].rumble_frames.append(frame)
+        if file_name in result:
+            sample, frame = convert_time(row, result[file_name].audio_tensor, result[file_name].sample_rate, frame_step)
+            result[file_name].rumble_samples.append(sample)
+            result[file_name].rumble_frames.append(frame)
+            continue
 
         file_path = directory / file_name
         audio_bytes = tf.io.read_file(str(file_path))
@@ -71,9 +75,9 @@ def read_audio_files(index_path: Path, directory: Path) -> list[FileResult]:
         if "vehicle" in path:
             category |= Category.Vehicle
         
-        sample, frame = convert_time(row, sample_rate_int, frame_step)
+        sample, frame = convert_time(row, audio, sample_rate_int, frame_step)
 
-        result.append(FileResult(
+        result[file_name] = FileResult(
             file_name=file_name,
             audio_tensor=audio,
             spectrogram_tensor=spectrogram, 
@@ -83,12 +87,13 @@ def read_audio_files(index_path: Path, directory: Path) -> list[FileResult]:
             rumble_samples=[sample],
             rumble_frames=[frame],
             call_type=row["Call_type"]
-        ))
-    return result
+        )
+    return list(result.values())
 
 def reconstruct_audio_tensor(audio_orig: tf.Tensor, new_spectrogram: tf.Tensor) -> tf.Tensor:
     # Original audio
-    stft_original = tf.signal.stft(audio_orig, frame_length=512, frame_step=256, fft_length=512)
+    global frame_length, frame_step, fft_length
+    stft_original = tf.signal.stft(audio_orig, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length)
 
     # Modified spectrogram (mel, filtered, etc.)
     modified_magnitude = tf.abs(new_spectrogram)  # [257, time_frames]
@@ -98,7 +103,7 @@ def reconstruct_audio_tensor(audio_orig: tf.Tensor, new_spectrogram: tf.Tensor) 
 
     # Back to audio:
     audio_reconstruct = tf.signal.inverse_stft(
-        stft_modified, frame_length=512, frame_step=256, fft_length=512
+        stft_modified, frame_length=frame_length, frame_step=frame_step, fft_length=fft_length
     )
     return audio_reconstruct
 
@@ -108,11 +113,7 @@ def save_reconstructed_audio(audio_tensor: tf.Tensor, sample_rate: int, filename
     audio_mono = tf.cast(audio_mono, tf.float32)
     
     # Encode as 16-bit PCM WAV
-    wav_bytes = tf.audio.encode(
-        audio_mono, 
-        sample_rate, 
-        name='encoded_wav'
-    )
+    wav_bytes = tf.audio.encode_wav(audio_mono, sample_rate)
     
     # Save to file
     tf.io.write_file(filename, wav_bytes)
@@ -121,17 +122,17 @@ def convert_to_mel(file_res: FileResult):
     num_spectrogram_bins = file_res.spectrogram_bins
     lower_edge_hertz, upper_edge_hertz, num_mel_bins = 0.0, 1000.0, 80
     linear_to_mel_weight_matrix = tf.signal.linear_to_mel_weight_matrix(
-    num_mel_bins, num_spectrogram_bins, file_res.sample_rate, lower_edge_hertz,
-    upper_edge_hertz)
+        num_mel_bins, num_spectrogram_bins, file_res.sample_rate, lower_edge_hertz,
+        upper_edge_hertz)
     mel_spectrograms = tf.tensordot(
-    file_res.spectrogram_tensor, linear_to_mel_weight_matrix, 1)
+        file_res.spectrogram_tensor, linear_to_mel_weight_matrix, 1)
     mel_spectrograms.set_shape(file_res.spectrogram_tensor.shape[:-1].concatenate(
-    linear_to_mel_weight_matrix.shape[-1:]))
+        linear_to_mel_weight_matrix.shape[-1:]))
     file_res.spectrogram_tensor = mel_spectrograms
 
 if __name__ == "__main__":
     directory = Path(__file__).parent / "data"
     index = Path(__file__).parent / "index.csv"
     results = read_audio_files(index, directory)
-    #print("Results:", results)
-    convert_to_mel(results[0])
+    print(len(results))
+    #convert_to_mel(results[0])
